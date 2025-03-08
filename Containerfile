4d346b340ae71ac94c9e7aa5e6ce3c4fccaed114
Sylvain Maret <sylvain.maret@hotmail.fr>
2025-03-08 15:29:45 +0000

    build against bluefin and try to install lact

--- Containerfile
+++ Containerfile
@@ -1,9 +1,9 @@
-FROM ghcr.io/ublue-os/silverblue-main:latest
+FROM ghcr.io/ublue-os/bluefin-dx:stable
 
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:stable
 # FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
+#
 # ... and so on, here are more base images
 # Universal Blue Images: https://github.com/orgs/ublue-os/packages
 # Fedora base image: quay.io/fedora/fedora-bootc:41
@@ -18,4 +18,3 @@ COPY build.sh /tmp/build.sh
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     ostree container commit
-    
--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+# dnf install -y tmux
 
 # Use a COPR Example:
 #
@@ -21,4 +21,33 @@ dnf install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+
+#!/usr/bin/bash
+
+
+#### Customization
+
+# Enabling lact on bluefin
+
+IMAGE_INFO="/usr/share/ublue-os/image-info.json"
+BASE_IMAGE_NAME=$(jq -r '."base-image-name"' < $IMAGE_INFO)
+ublue-update --wait
+rpm-ostree kargs --append-if-missing=$(printf 'amdgpu.ppfeaturemask=0x%x\n' "$(($(cat /sys/module/amdgpu/parameters/ppfeaturemask) | 0x4000))")
+if [[ ${BASE_IMAGE_NAME} == 'silverblue' ]]; then
+    echo 'Installing LACT Libadwaita...'
+    wget \
+      $(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | \
+      jq -r ".assets[] | select(.name | test(\"lact-libadwaita.*fedora-$(rpm -E %fedora)\")) | .browser_download_url") \
+      -O /tmp/lact.rpm
+else
+    echo 'Installing LACT...'
+    wget \
+      $(curl -s https://api.github.com/repos/ilya-zlobintsev/LACT/releases/latest | \
+      jq -r ".assets[] | select(.name | test(\"lact-[0-9].*fedora-$(rpm -E %fedora)\")) | .browser_download_url") \
+      -O /tmp/lact.rpm
+fi
+rpm-ostree install --apply-live -y /tmp/lact.rpm
+sudo systemctl enable --now lactd
+rm /tmp/lact.rpm
+echo 'Complete.'
