9ffbc3ecf147fc668283b28f1451e3f5da87b54d
Sylvain Maret <sylvain.maret@hotmail.fr>
2025-05-14 21:19:41 +0000

    add a cleanup of dnf

--- build.sh
+++ build.sh
@@ -18,11 +18,11 @@ set -ouex pipefail
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-dnf5 -y copr enable ilyaz/LACT
-dnf5 -y install lact libvirt-devel mangohud pipx
+dnf -y copr enable ilyaz/LACT
+dnf -y install lact libvirt-devel mangohud pipx
 systemctl enable lactd
-dnf5 -y copr disable ilyaz/LACT
-
+dnf -y copr disable ilyaz/LACT
+dnf clean all
 #### Example for enabling a System Unit File
 
 # systemctl enable podman.socket
