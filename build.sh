392545f0f99fb2b02e66e1023d5251a0a19ee7f1
SylvainSesterce <sylvain@sesterce.com>
2025-07-01 15:46:19 +0200

    [FEAT] Add keepassxc rpm

--- build.sh
+++ build.sh
@@ -20,8 +20,8 @@ set -ouex pipefail
 # dnf5 -y copr disable ublue-os/staging
 dnf -y copr enable ilyaz/LACT
 dnf -y copr enable praiskup/safeeyes
-dnf -y copr enable dsommers/openvpn3
-dnf -y install lact libvirt-devel mangohud pipx python3-safeeyes openvpn3-client
+dnf -y install https://packages.openvpn.net/openvpn-openvpn3-epel-repo-1-1.noarch.rpm
+dnf -y install lact libvirt-devel mangohud pipx python3-safeeyes openvpn3-client keepassxc
 systemctl enable lactd
 dnf -y copr disable ilyaz/LACT
 dnf -y copr disable praiskup/safeeyes
