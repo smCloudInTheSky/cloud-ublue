d7296717a246d54a10894fe189fb623ce6e4da48
smCloudInTheSky <sylvain.maret@hotmail.fr>
2025-08-11 21:55:00 +0200

    thinkpad : fix install cmd

--- build_thinkpad.sh
+++ build_thinkpad.sh
@@ -7,7 +7,7 @@ set -ouex pipefail
 dnf -y copr enable abn/throttled
 dnf -y copr enable sneexy/python-validity
 dnf -y remove tuned tuned-ppd
-dnf -y install --allowerasing gstreamer1-plugin-vaapi libheif igt-gpu-tools fprintd-clients fprintd-clients-pam open-fprintd python3-validity tlp tlp-rdw throttled zcfan alsa-firmware ffmpegthumbnailer heif-pixbuf-loader libcamera-gstreamer libcamera-tools libva-utils pipewire-plugin-vulkan vulkan-headers dnf -y group install sound-and-video mesa-dri-drivers mesa-filesystem mesa-libEGL mesa-libGL mesa-libgbm mesa-va-drivers mesa-vulkan-drivers
+dnf -y install --allowerasing gstreamer1-plugin-vaapi libheif igt-gpu-tools fprintd-clients fprintd-clients-pam open-fprintd python3-validity tlp tlp-rdw throttled zcfan alsa-firmware ffmpegthumbnailer heif-pixbuf-loader libcamera-gstreamer libcamera-tools libva-utils pipewire-plugin-vulkan vulkan-headers mesa-dri-drivers mesa-filesystem mesa-libEGL mesa-libGL mesa-libgbm mesa-va-drivers mesa-vulkan-drivers
 dnf -y swap --from-repo=fedora-multimedia ffmpeg-free ffmpeg
 dnf -y swap --from-repo=fedora-multimedia libavcodec-free libavcodec
 dnf -y swap --from-repo=fedora-multimedia libfdk-aac-free libfdk-aac
