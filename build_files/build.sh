2b3198865043243fd66ed6c886889a48aca506a0
SylvainSesterce <sylvain@sesterce.com>
2025-08-14 09:14:58 +0000

    add acpi tool + edid

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ set -ouex pipefail
 # dnf5 -y copr disable ublue-os/staging
 dnf -y copr enable ilyaz/LACT
 dnf -y copr enable praiskup/safeeyes
-dnf -y install lact libvirt-devel mangohud pipx python3-safeeyes keepassxc firefox git-lfs clustershell vmaf-models vmaf libvmaf-devel s-tui rasdaemon
+dnf -y install lact libvirt-devel mangohud pipx python3-safeeyes keepassxc firefox git-lfs clustershell vmaf-models vmaf libvmaf-devel s-tui rasdaemon acpica-tools edid-decode
 systemctl enable lactd
 systemctl enable rasdaemon
 dnf -y copr disable ilyaz/LACT
