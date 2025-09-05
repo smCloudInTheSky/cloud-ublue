f1bcfa02ea44d59e83f97849fcc2ebfe5951a69b
Sylvain Maret <sylvain.maret@hotmail.fr>
2025-09-05 10:11:32 +0000

    try to remove unneeded packages

--- build_files/build_thinkpad.sh
+++ build_files/build_thinkpad.sh
@@ -5,16 +5,13 @@ set -ouex pipefail
 dnf -y copr enable abn/throttled
 dnf -y copr enable sneexy/python-validity
 dnf -y remove tuned tuned-ppd
-dnf -y install --allowerasing gstreamer1-plugin-vaapi libheif igt-gpu-tools fprintd-clients fprintd-clients-pam open-fprintd python3-validity tlp tlp-rdw throttled zcfan alsa-firmware ffmpegthumbnailer heif-pixbuf-loader libcamera-gstreamer libcamera-tools libva-utils pipewire-plugin-vulkan vulkan-headers mesa-dri-drivers mesa-filesystem mesa-libEGL mesa-libGL mesa-libgbm mesa-va-drivers mesa-vulkan-drivers
-dnf -y swap --from-repo=fedora-multimedia ffmpeg-free ffmpeg
-dnf -y swap --from-repo=fedora-multimedia libavcodec-free libavcodec
-dnf -y swap --from-repo=fedora-multimedia libfdk-aac-free libfdk-aac
+dnf -y install --allowerasing igt-gpu-tools fprintd-clients fprintd-clients-pam open-fprintd python3-validity tlp tlp-rdw throttled zcfan
 
 dnf -y copr disable abn/throttled
 dnf -y copr disable sneexy/python-validity
 dnf clean all
 
-echo -e "\n com.github.d4nj1.tlpui" >> /etc/ublue-os/system-flatpaks.list
+echo -e "\ncom.github.d4nj1.tlpui" >> /etc/ublue-os/system-flatpaks.list
 systemctl enable tlp.service
 systemctl enable zcfan.service
 systemctl enable throttled.service
