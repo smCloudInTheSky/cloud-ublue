f570d2967735cbbbf563f8443694bcb51a3d7300
Sylvain Maret <sylvain.maret@hotmail.fr>
2025-12-15 12:15:39 +0000

    [FEAT] add libbpf

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,7 +20,7 @@ set -ouex pipefail
 # dnf5 -y copr disable ublue-os/staging
 dnf -y copr enable ilyaz/LACT
 dnf -y copr enable praiskup/safeeyes
-dnf -y install lact libvirt-devel mangohud pipx keepassxc firefox git-lfs clustershell vmaf-models vmaf libvmaf-devel s-tui rasdaemon acpica-tools edid-decode telnet libreswan edk2-ovmf
+dnf -y install lact libvirt-devel mangohud pipx keepassxc firefox git-lfs clustershell vmaf-models vmaf libvmaf-devel s-tui rasdaemon acpica-tools edid-decode telnet libreswan edk2-ovmf libbpf-devel libbpf
 
 #### Example of preparation for installing a package that requires a symlinked directory
 
