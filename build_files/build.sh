a4bdd6938bb7d1ba8604d55561d407d237fef3c6
Sylvain Maret <sylvain.maret@hotmail.fr>
2026-05-27 08:54:57 +0000

    testing

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,20 +19,8 @@ set -ouex pipefail
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 dnf -y copr enable ilyaz/LACT
-dnf -y copr enable praiskup/safeeyes
-dnf -y copr enable bieszczaders/kernel-cachyos
-dnf -y copr enable bieszczaders/kernel-cachyos-addons
-# prepare for cachyos kernel setup
-setsebool -P domain_kernel_load_modules on
-dracut -f
-# switch to cachyos kernel
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos
 # install extra packages
-dnf -y install lact libvirt-devel mangohud pipx python3-safeeyes keepassxc firefox git-lfs clustershell vmaf-models vmaf libvmaf-devel https://github.com/ebkr/r2modmanPlus/releases/download/v3.2.3/r2modman-3.2.3.x86_64.rpm
-# scx userspace scheduler + tooling
-dnf -y install scx-scheds scx-tools scx-manager
-# switch to cachyos default settings
-dnf -y swap zram-generator-defaults cachyos-settings
+dnf -y install lact libvirt-devel mangohud pipx keepassxc firefox git-lfs clustershell vmaf-models vmaf libvmaf-devel https://github.com/ebkr/r2modmanPlus/releases/download/v3.2.3/r2modman-3.2.3.x86_64.rpm
 
 #### Example of preparation for installing a package that requires a symlinked directory
 
@@ -43,11 +31,6 @@ ln -sr /opt /var/opt
 # install package (dnf5 -y install .....)
 dnf install -y https://github.com/ebkr/r2modmanPlus/releases/download/v3.2.3/r2modman-3.2.3.x86_64.rpm
 dnf install -y https://github.com/skevetter/devpod/releases/latest/download/DevPod_linux_x86_64.rpm
-
-# scx userspace scheduler + tooling
-dnf -y install scx-scheds scx-tools scx-manager
-# switch to cachyos default settings
-dnf -y swap zram-generator-defaults cachyos-settings
 
 cat <<-EOF | tee /etc/yum.repos.d/netbird.repo
 [NetBird]
@@ -73,9 +56,6 @@ systemctl enable lactd
 systemctl enable rasdaemon
 # Zoom install because zoom is broken
 dnf -y copr disable ilyaz/LACT
-dnf -y copr disable praiskup/safeeyes
-dnf -y copr disable bieszczaders/kernel-cachyos
-dnf -y copr disable bieszczaders/kernel-cachyos-addons
 dnf clean all
 #### Example for enabling a System Unit File
 
